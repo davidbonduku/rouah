834e9ac90c89acd78404560d6fd0fba519962364
davidbonduku <david_bonduku@yahoo.fr>
2015-10-29 02:35:08 +0100

    Update CoreModel.php

--- backend/core/CoreModel.php
+++ backend/core/CoreModel.php
@@ -1,20 +1,6 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: davidbondukupieme
- * Date: 22/10/15
- * Time: 17:43
- */
 class CoreModel
 {
-    static function getAll()
-    {
-        return "Je suis avec Ibrahim, nous bossons sur PHP en version Pro";
-    }
-
-    static function getUserId()
-    {
-        return uniqid();
-    }
+  
 }
