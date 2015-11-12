66734ea82c83e226e456ce9d583e37d3e6157606
davidbonduku <david_bonduku@yahoo.fr>
2015-11-12 14:30:53 +0100

    mise à jour

--- backend/core/CoreController.php
+++ backend/core/CoreController.php
@@ -1,64 +1,101 @@
 <?php
 
-class CoreController
-{
-    /**
-     * Permet de charger un controlleur
-     * @param $controllerName
-     * @return mixed
-     */
-
-   static function loadController($controllerName)
-    {
-        require ROOT.'controllers'.DS.$controllerName.'.php';
-    }
-    /**
-     * Permet de passer les données à la vue
-     * @param array $data
-     */
-    public function _setView(array $data)
-    {
-        $this->_loadView(array(
-                                'viewFileName' => $data["view"],
-                                "content" => $data['content'])
-        );
-    }
-    /**
-     * Permet de charger la vue
-     * @param array $viewData
-     */
-    private function _loadView(array $viewData)
-    {
-        extract($viewData);
-
-        require ROOT.'views'.DS.$viewData["viewFileName"].'.php';
-    }
-    /**
-     * Permet de convertir en JSON
-     * @param $data
-     * @return string
-     */
-    protected function _convertToJson($data)
-    {
-        return json_encode($data);
-    }
-    /**
-     * Permet de récupérer les données distantes
-     * @return string|void
-     */
-    protected function _getHttpData()
-    {
+/**
+ * Class CoreController
+ */
+ class CoreController
+ {
+     protected static $_currentModel;
+     /**
+      * Permet de charger un contrôleur et d'instancier son modèle associé
+      * Permet de charger un controlleur
+      * @param $controllerName
+      * @return mixed
+      */
+     static function loadController($controllerName)
+     {
+               if( self::_checkFileAndLoad(ROOT.'controllers'.DS.$controllerName.'Controller.php') )
+                {
+                       self::loadModel( $controllerName );
+                }
+         require ROOT.'controllers'.DS.$controllerName.'.php';
+     }
+     /**
+      * Permet de passer les données à la vue
+      * @param array $data
+      */
+     public function _setView(array $data)
+     {
+         $this->_loadView(array(
+             'content' => $data['content'],
+             'viewFileName' => $data["view"],
+            ));
+     }
+     /**
+      * Permet de charger la vue
+      * @param array $viewData
+      */
+     private function _loadView(array $viewData)
+     {
+         ob_start();
+         extract($viewData);
+         require_once ROOT.'views'.DS.$viewData['viewFileName'].'.php';
+         ob_clean();
+         require ROOT.'views'.DS.$viewData["viewFileName"].'.php';
+     }
+     /**
+      * Permet de convertir en JSON
+      * @param $data
+      * @return string
+      */
+     protected function _convertToJson($data)
+     {
+         return json_encode($data);
+     }
+     /**
+      * Permet de récupérer les données distantes
+      * @return string|void
+      */
+     protected function _getHttpData()
+     {
          switch($_SERVER['REQUEST_METHOD'])
          {
              case 'POST':
                  return $_POST;
-             break;
+                 break;
              case 'GET':
                  return $_GET;
-             break;
+                 break;
              case 'PUT':
                  return file_get_contents('php://input');
          }
-        return;
-    }
-}
+         return;
+     }
+     /**
+       * Permet de charger le modèle
+      * @param $modelName
+       */
+      static function loadModel($modelName)
+     {
+         if(self::_checkFileAndLoad(ROOT.'models'.DS.$modelName.'Model.php'))
+         {
+             $currentModel = $modelName.'Model';
+            self::$_currentModel = new $currentModel();
+         }
+     }
+     /**
+      * Verifie si le fichier existe et le charge
+      * @param $fileName
+      * @return bool
+      */
+     private static function _checkFileAndLoad( $fileName )
+     {
+             if(file_exists($fileName))
+                 {
+                     require_once $fileName;
+
+                     return true;
+         }
+          return false;
+     }
+ }
