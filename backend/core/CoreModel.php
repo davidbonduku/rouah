1a68fc74026a367f5823c3733d0d55068c87afec
ozo <dribro07@yahoo.fr>
2015-11-19 19:18:25 +0100

    mise à jour  methode add

--- backend/core/CoreModel.php
+++ backend/core/CoreModel.php
@@ -36,4 +36,33 @@ class CoreModel
         return $res->fetchAll(PDO::FETCH_OBJ);
     }
 
+    protected function _add(array $data)
+    {
+        if(sizeof($data) == 0 )
+        {
+            AppException::show(array(
+                'message'=> "Impossible d'ajouter données",
+                'type'=> "DataModelException",
+                'code'=> 209
+            ));
+
+        }else{
+                 $sql = "INSERT INTO $this->_table (";
+
+                 $sqlTemp = ""; $valuesTemp = "";
+
+                foreach($data as $key=>$value){
+
+                    $sqlTemp.=$key.",";
+
+                    $valuesTemp.=":".$key.",";
+                }
+
+                $request = $this->_db->prepare( $sql.substr($sqlTemp,0,-1).")
+
+                                               VALUES (".substr($valuesTemp,0,-1).")"
+                );
+                $request->execute($data);
+        }
+    }
 }
